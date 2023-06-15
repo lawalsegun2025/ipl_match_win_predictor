f2885b3761cc6aa05bc2f7d719bebb5a8666a1bc
lawalsegun2025 <lawalsegun2025@gmail.com>
2023-06-15 11:03:27 +0100

    importing the model

---
+++ app.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import pandas as pd 
+import pickle
+
+# Declaring the most common teams 
+teams = ['Sunrisers Hyderabad',
+         'Mumbai Indians',
+         'Royal Challengers Bangalore',
+         'Kolkata Knight Riders',
+         'Kings XI Punjab',
+         'Chennai Super Kings',
+         'Rajasthan Royals',
+         'Delhi Capitals']
+
+# Delcaring the venues (cities) of the matches
+cities = ['Hyderabad', 'Bangalore', 'Mumbai', 'Indore', 'Kolkata', 'Delhi',
+          'Chandigarh', 'Jaipur', 'Chennai', 'Cape Town', 'Port Elizabeth',
+          'Durban', 'Centurion', 'East London', 'Johannesburg', 'Kimberley',
+          'Bloemfontein', 'Ahmedabad', 'Cuttack', 'Nagpur', 'Dharamsala',
+          'Visakhapatnam', 'Pune', 'Raipur', 'Ranchi', 'Abu Dhabi',
+          'Sharjah', 'Mohali', 'Bengaluru']
